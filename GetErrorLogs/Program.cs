b8346b99e43ff31d1adb5df9f945f205f5df1cb5
varun brahmane <varun@seligentconsulting.com>
2022-12-10 15:14:43 +0530

    Code for get a file and read a file throgh Stream reader.

--- GetErrorLogs/Program.cs
+++ GetErrorLogs/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,27 @@ namespace GetErrorLogs
     {
         static void Main(string[] args)
         {
-
+            try
+            {
+                string filePath = ConfigurationManager.AppSettings["ErrorLogFilePath"];
+                var fileData= InternalReadAllText(filePath, Encoding.UTF8);
+                Console.WriteLine("File data : \n " + fileData);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey(true);
         }
+        public static string InternalReadAllText(string path, Encoding encoding)
+        {
+            string result;
+            using (StreamReader streamReader = new StreamReader(path, encoding))
+            {
+                result = streamReader.ReadToEnd();
+            }
+            return result;
+        }
+        
     }
 }
